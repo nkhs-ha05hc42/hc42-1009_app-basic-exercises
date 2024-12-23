7c58c02a9dcc8613f0babc0688feabf0afd8d2a9
hc421009 <nkhshc421009@g.neec.ac.jp>
2024-12-23 13:38:31 +0900

    6-3-1

--- 6-3/6-3-1.mjs
+++ 6-3/6-3-1.mjs
@@ -1,6 +1,4 @@
 const array = ["aaa","bbb","ccc"]
-const Newarray = array.map(value =>
-({key: `★${value}`
-}))
+const Newarray = array.map(array => '★' + array) 
 
 console.log(Newarray[0],Newarray[1],Newarray[2])
