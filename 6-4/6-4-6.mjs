f8ca721d5895c8a3ed3144c32ce7440d2c9765dd
hc421009 <nkhshc421009@g.neec.ac.jp>
2024-12-23 14:07:53 +0900

    6-4-6

---
+++ 6-4/6-4-6.mjs
@@ -0,0 +1,13 @@
+const value = "地獄谷"
+switch (value) {
+    case "地獄谷":
+    case "気仙沼":
+        console.log("登別")
+        break
+    case "ウポポイ":
+    case "白老牛":
+        console.log("白老")
+        break
+    default:
+        console.log("対象外")
+}
