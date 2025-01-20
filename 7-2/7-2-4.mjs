24490600ccd904f325a346c28a504fc6241bd23e
hc421009 <nkhshc421009@g.neec.ac.jp>
2025-01-20 13:15:51 +0900

    7-2-4

---
+++ 7-2/7-2-4.mjs
@@ -0,0 +1,6 @@
+import {kake} from "./7-2-4_export1.mjs"
+import {waru} from "./7-2-4_export2.mjs"
+const result1 = kake(3, 5)
+console.log("kake", result1)
+const result2 = waru (6, 2)
+console.log("waru", result2)
