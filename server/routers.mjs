1148536429c515614f24f91ba508520a792a0197
hc421009 <nkhshc421009@g.neec.ac.jp>
2025-02-10 09:39:53 +0900

    9-7

--- server/routers.mjs
+++ server/routers.mjs
@@ -1,19 +1,32 @@
 import express from "express"
 import path from "path"
-import { sample1Controller } from "./api/controllers.mjs"
-import { q8_1Controller } from "./api/controllers.mjs"
-
+import {
+    q9_7controller,
+    sample1Controller,
+    sample2Controller
+ } from "./api/controllers.mjs"
+import { q8_1Controller  } from "./api/8-1/controller.mjs"
 const routers = express.Router()
-
 routers.post("/api/sample1", sample1Controller.postSample1)
 routers.get("/api/sample1", sample1Controller.getSample1)
-routers.post("/api/8-1", q8_1Controller.post8_1)
-routers.get("/api/8-1", q8_1Controller.get8_1)
+routers.get("/api/sample1/:id", sample1Controller.getPathParamSample1)
 routers.put("/api/sample1", sample1Controller.putSample1)
 routers.delete("/api/sample1", sample1Controller.deleteSample1)
-routers.get("/api/sample1/:id", sample1Controller.getPathParamSample1)
+
+routers.get("/api/8-1", q8_1Controller.get8_1)
+routers.get("/api/8-1", q8_1Controller.post8_1)
+
+routers.post("/api/sample2", sample2Controller.postSample2)
+routers.get("/api/sample2/:id", sample2Controller.getOneSample2)
+routers.get("/api/sample2", sample2Controller.getAllSample2)
+routers.put("/api/sample2/:id", sample2Controller.putSample2)
+routers.delete("/api/sample2/:id", sample2Controller.deleteSample2)
+
+routers.post("/api/9-7", q9_7controller.postq9_7)
+routers.get("/api/9-7/:id", q9_7controller.getTwoq9_7)
+routers.get("/api/9-7", q9_7controller.getAllq9_7)
+routers.put("/api/9-7/:id", q9_7controller.putq9_7)
 
 // client配下は、ブラウザサイドで実行されるファイル（HTML/JS/CSS/画像など）を配置する
 routers.use(express.static(`${path.resolve()}/client`))
-
 export default routers
